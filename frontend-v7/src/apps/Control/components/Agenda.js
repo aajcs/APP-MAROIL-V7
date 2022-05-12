4987e7022d894b7bf336aaed16dc6411ca15bd78
aajcs <ing.alfredoacastillo@gmail.com>
2022-05-11 20:09:26 -0400

    culminado el card de agenda

--- frontend-v7/src/apps/Control/components/Agenda.js
+++ frontend-v7/src/apps/Control/components/Agenda.js
@@ -1,10 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable array-callback-return */
 /* eslint-disable multiline-ternary */
 /* eslint-disable prefer-const */
 /* eslint-disable indent */
 /* eslint-disable new-cap */
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { ProgramacionVentanaContext } from '../contexts/ProgramacionVentanaContext'
 import { Skeleton } from 'primereact/skeleton'
+import moment from 'moment'
 
 import ProgramacionVentanaAngenaV2 from './ProgramacionVentanaAngenaV2'
 
@@ -13,6 +16,10 @@ import ProgramacionVentanaCard from './ProgramacionVentanaCard'
 
 const Agenda = () => {
   const { programacionVentanas } = useContext(ProgramacionVentanaContext)
+  const [terminalMaroilPuesto1, setTerminalMaroilPuesto1] = useState([])
+  const [terminalMaroilPuesto2, setTerminalMaroilPuesto2] = useState([])
+  const [terminalPetroSanFelix, setTerminalPetroSanFelix] = useState([])
+  const [terminalPetroCedeno, setTerminalPetroCedeno] = useState([])
 
   //   const schedulerData = new SchedulerData(
   //     new moment().format(DATE_FORMAT),
@@ -22,6 +29,10 @@ const Agenda = () => {
   //   schedulerData.setLocaleMoment(moment)
 
   let auxProgramacionVentanas = []
+  let auxTerminalMaroilPuesto1 = []
+  let auxTerminalMaroilPuesto2 = []
+  let auxTerminalPetroSanFelix = []
+  let auxTerminalPetroCedeno = []
   for (let prop in programacionVentanas) {
     auxProgramacionVentanas.push({
       id: programacionVentanas[prop].id,
@@ -48,7 +59,38 @@ const Agenda = () => {
           : '#dc3545'
     })
   }
+  const agentaCard = () => {
+    programacionVentanas.map((events) => {
+      if (events.fechaFinVentana >= moment().subtract(1, 'days').format()) {
+        if (events.terminalBuque === 'MAROIL TERMINAL 1') {
+          console.log('1', events.terminalBuque)
+          auxTerminalMaroilPuesto1.push(events)
+        } else if (events.terminalBuque === 'MAROIL TERMINAL 2') {
+          console.log('2', events.terminalBuque)
+          auxTerminalMaroilPuesto2.push(events)
+        } else if (events.terminalBuque === 'PETRO SAN FELIX') {
+          console.log('3', events.terminalBuque)
+          auxTerminalPetroSanFelix.push(events)
+        } else {
+          console.log('4', events.terminalBuque)
+          auxTerminalPetroCedeno.push(events)
+        }
+      }
+    })
+    setTerminalMaroilPuesto1(auxTerminalMaroilPuesto1)
+    setTerminalMaroilPuesto2(auxTerminalMaroilPuesto2)
+    setTerminalPetroSanFelix(auxTerminalPetroSanFelix)
+    setTerminalPetroCedeno(auxTerminalPetroCedeno)
+  }
   console.log(auxProgramacionVentanas)
+  console.log(auxTerminalMaroilPuesto1)
+  console.log(auxTerminalMaroilPuesto2)
+  console.log(auxTerminalPetroSanFelix)
+  console.log(auxTerminalPetroCedeno)
+  useEffect(() => {
+    agentaCard()
+  }, [programacionVentanas])
+
   return (
     <div>
       {auxProgramacionVentanas.length === 0 ? (
@@ -74,21 +116,66 @@ const Agenda = () => {
           <div className="card">
             <ProgramacionVentanaAngenaV2 events={auxProgramacionVentanas} />
           </div>
-          <div className="grid flex">
-            {programacionVentanas.map((events) => (
-              <div key={events.id} className="col-12 lg:col-6 xl:col-3">
-                <div className="card  ">
-                  <span className="text-green-500 text-center fw-bold fst-italic mb-2">
-                    {events.terminalBuque}
-                  </span>
-
-                  <ProgramacionVentanaCard key={events.id} events={events} />
-                </div>
-              </div>
-            ))}{' '}
-          </div>
         </>
       )}
+      <div className="grid flex">
+        {auxTerminalMaroilPuesto1.length === 0 && (
+          <div className="col-12 lg:col-6 xl:col-3">
+            <div className="card  ">
+              <span className="text-900 text-center fw-bold fst-italic mb-2">
+                MAROIL TERMINAL 1
+              </span>
+              {terminalMaroilPuesto1.map((events) => (
+                <>
+                  <ProgramacionVentanaCard key={events.id} events={events} />
+                </>
+              ))}
+            </div>
+          </div>
+        )}
+        {auxTerminalMaroilPuesto2.length === 0 && (
+          <div className="col-12 lg:col-6 xl:col-3">
+            <div className="card  ">
+              <span className="text-900 text-center fw-bold fst-italic mb-2">
+                MAROIL TERMINAL 2
+              </span>
+              {terminalMaroilPuesto2.map((events) => (
+                <>
+                  <ProgramacionVentanaCard key={events.id} events={events} />
+                </>
+              ))}
+            </div>
+          </div>
+        )}
+        {auxTerminalPetroSanFelix.length === 0 && (
+          <div className="col-12 lg:col-6 xl:col-3">
+            <div className="card  ">
+              <span className="text-900 text-center fw-bold fst-italic mb-2">
+                PETRO SAN FELIX
+              </span>
+              {terminalPetroSanFelix.map((events) => (
+                <>
+                  <ProgramacionVentanaCard key={events.id} events={events} />
+                </>
+              ))}
+            </div>
+          </div>
+        )}
+        {auxTerminalPetroCedeno.length === 0 && (
+          <div className="col-12 lg:col-6 xl:col-3">
+            <div className="card  ">
+              <span className="text-900 text-center fw-bold fst-italic mb-2">
+                PETRO SEDEÑO
+              </span>
+              {terminalPetroCedeno.map((events) => (
+                <>
+                  <ProgramacionVentanaCard key={events.id} events={events} />
+                </>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
--- frontend-v7/src/apps/Control/components/ProgramacionVentanaCard.js
+++ frontend-v7/src/apps/Control/components/ProgramacionVentanaCard.js
@@ -3,34 +3,64 @@ import React from 'react'
 import moment from 'moment'
 
 function ProgramacionVentanaCard({ events }) {
+  const consignatarioBuque = {
+    MAROIL: '#2600ff',
+    PDVSA: '#ff0000'
+  }
+
+  const consignatarioBuqueColor = consignatarioBuque[events.buqueCliente]
+  const ubicacionBuqueTags = {
+    'MAROIL TERMINAL 1': '#094db1',
+    'MAROIL TERMINAL 2': '#094db1',
+    'PETRO CEDENO': '#d9a406',
+    'PETRO SAN FELIX': '#157347'
+  }
+
+  const ubicacionBuqueTag = ubicacionBuqueTags[events.terminalBuque]
   console.log(events)
   return (
     // <div className="col-12 lg:col-6 xl:col-3">
-    <div className="card mb-0 border-top-0 border-end-0  border-danger  border-2 ">
-      <div className="flex justify-content-between">
-        <div>
-          <div className="text-900 font-medium text-xl">
-            {events.nombreBuque}
+    <>
+      <div
+        className="card Pb-0 border-bottom-0 border-start-0 border-2  mb-0 text-end"
+        style={{ 'border-color': ubicacionBuqueTag, padding: 0 }}
+      >
+        <span className="mr-2  fst-italic ">
+          {events.fechaInicioVentana <= moment().add(1, 'days').format()
+            ? 'Buque Actual en Ventana'
+            : 'Buque Proximo en Ventana'}
+        </span>
+      </div>
+
+      <div
+        className="card Pb-2 border-top-0 border-end-0 border-2 "
+        style={{ 'border-color': consignatarioBuqueColor }}
+      >
+        <div className="flex justify-content-between">
+          <div>
+            <div className="text-900 font-medium text-xl">
+              {events.nombreBuque}
+            </div>
+          </div>
+          <div
+            className="flex align-items-center justify-content-center  border-round ml-2"
+            style={{ height: '2.5rem', 'font-size': '0.8rem' }}
+          >
+            {new Intl.NumberFormat().format(events.toneladasNominadas)}{' '}
+            {' TM / '} {events.buqueCliente}
           </div>
         </div>
-        <div
-          className="flex align-items-center justify-content-center  border-round ml-2"
-          style={{ height: '2.5rem', 'font-size': '0.8rem' }}
-        >
-          {new Intl.NumberFormat().format(events.toneladasNominadas)} {' TM / '}{' '}
-          {events.buqueCliente}
-        </div>
-      </div>
-      {/* <span className="text-green-500 font-medium">
+        {/* <span className="text-green-500 font-medium">
           <p className=" mb-0">{events.terminalBuque}</p>
         </span> */}
-      <span className="text-500">
-        {' '}
-        {moment(events.fechaInicioVentana).format('DD-MMMM')} {' => '}{' '}
-        {moment(events.fechaFinVentana).format('DD-MMMM')}{' '}
-      </span>
-      <span className="text-end p-0"></span>
-    </div>
+        <span className="text-500">
+          {' '}
+          {moment(events.fechaInicioVentana).format('DD-MMMM')} {' => '}{' '}
+          {moment(events.fechaFinVentana).format('DD-MMMM')}{' '}
+        </span>
+        <span className="text-end p-0"></span>
+      </div>
+    </>
     // </div>
   )
 }
